d6086c88a7b0d74ea284fda9c3e2333501877e64
Andrés Palumbo <andrespalumbo@gmail.com>
2016-10-04 22:06:34 -0300

    [Andrés Palumbo] - Exercise 1 - Pending some important functionality

---
+++ 03-MVC-AngularJS/scripts/angular.js
@@ -0,0 +1,98 @@
+var myApp = angular.module('myApp', []);
+
+myApp.factory('movieClick', () => {
+    let movieInfo = {};
+    return movieInfo;
+});
+
+myApp.controller('MoviesCtrl', ['$scope', '$rootScope', ($rootScope, $scope, movieClick) => {
+    
+    $scope.movies = {};
+    $scope.movies.list = angular.fromJson(localStorage.getItem("movieList"));
+
+    $scope.update = () => {
+        $scope.movies.list = angular.fromJson(localStorage.getItem("movieList"));
+    }
+
+    $scope.showMovie = (movie) => {
+        //movieInfo.movie = movie;
+        //movieInfo.mode = "read";
+        $rootScope.$broadcast("showDetails", movie);
+    }
+
+    $scope.editMovie = (movie) => {
+        //movieInfo.movie = movie;
+        //movieInfo.mode = "readwrite";
+        $rootScope.$broadcast("edit", movie);
+    }
+
+    $scope.deleteMovie = (index) => {
+        $scope.movies.list.splice(index,1);
+    }
+
+    $rootScope.$on("addMovie", (event, data) => {
+        $scope.movies.list.push(data);
+    })
+
+}]);
+
+
+myApp.controller('adeCtrl', ['$scope','$rootScope', ($rootScope, $scope, movieClick) => {
+    $scope.isAdding = false;
+    $scope.isEditing = false;
+    $scope.showDetails = false;
+
+    $rootScope.$on("showDetails", (event, data) => {
+        $scope.showDetails = true;
+        $scope.movie = data;
+    })
+
+    $rootScope.$on("edit", (event, data) => {
+        $scope.isEditing = true;
+        $scope.movie = data;
+    })
+
+    $scope.addMovie = (movieToAdd) => {
+        $rootScope.$broadcast("addMovie", movieToAdd);
+    }
+
+}]);
+
+/*
+
+$scope.movies = {};
+
+    $scope.movies.list = [{
+        "title": "The Shawshank Redemption",
+        "year": "1994",
+        "duration": "142"
+    },{
+        "title": "Inception",
+        "year": "2010",
+        "duration": "148"
+    },{
+        "title": "The Hobbit: An Unexpected Journey",
+        "year": "2012",
+        "duration": "169"
+    },{
+        "title": "Back to the Future",
+        "year": "1985",
+        "duration": "116"
+    }];
+
+    $scope.isVisible = false;
+    $scope.isEditing = false;
+
+    $scope.deleteMovie = (index) => {
+        $scope.movies.list.splice(index,1);
+    }
+
+    $scope.addMovie = (movie) => {
+        $scope.movies.list.push(angular.copy(movie));
+    }
+
+    $scope.saveMovie = (movie, index) => {
+        $scope.movies.list[index] = angular.copy(movie);
+        console.log($scope.movies.list);
+    }
+*/
---
+++ 03-MVC-AngularJS/scripts/init.js
@@ -0,0 +1,17 @@
+//init some values in localStorage
+
+class Movie {
+    constructor(title, year, duration) {
+        this.title = title;
+        this.year = year;
+        this.duration = duration;
+    }
+}
+
+let movieList_init = [new Movie("The Shawshank Redemption", 1994, 142),
+                      new Movie("Inception", 2010, 148),
+                      new Movie("The Hobbit: An Unexpected Journey", 2012, 169),
+                      new Movie("Back to the Future", 1985, 116)
+                      ];    
+
+localStorage.setItem("movieList", JSON.stringify(movieList_init));
