27f5bbb440d19e2f38f25ec77b3146820cf04d95
Andrés Palumbo <andrespalumbo@gmail.com>
2016-10-21 20:08:48 -0300

    [Andrés Palumbo] - Fixed extra parameters in API calls

--- app/services/pvpLeaderboards.js
+++ app/services/pvpLeaderboards.js
@@ -1,12 +1,16 @@
 let pvpLeaderboards = angular.module('pvpLeaderboards',[]);
 
 pvpLeaderboards.factory('pvpLeaderboards', function($http, apiConfig) {
+    let params = {
+        apikey: apiConfig.apikey,
+        locale: apiConfig.locale,
+    }
 
     function getBracket(bracket) {
         return $http({
             method: 'GET',
             url: apiConfig.baseUrl + '/wow/leaderboard/'+bracket,
-            params: apiConfig,
+            params: params,
             timeout: 120000 //setting timeout because API usually takes to long to respond
         })
     };
--- app/services/realmInfo.js
+++ app/services/realmInfo.js
@@ -1,12 +1,16 @@
 let realmInfo = angular.module('realmInfo',[]);
 
 realmInfo.factory('realmInfo', function($http, apiConfig) {
+    let params = {
+        apikey: apiConfig.apikey,
+        locale: apiConfig.locale,
+    }
     
     function getList() {
         return $http({
             method: 'GET',
             url: apiConfig.baseUrl + '/wow/realm/status',
-            params: apiConfig
+            params: params
         })
     };
     
